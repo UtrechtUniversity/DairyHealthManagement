7adcb56e4408705c4b9f1a8d5f4fcd8f1463c346
Miel Hostens <m.m.hostens@uu.nl>
2023-08-15 15:49:33 +0200

    Remove the now ignore renv library directory

--- renv/library/R-4.2/x86_64-w64-mingw32/fivethirtyeight/doc/user_contributed_vignettes.R
+++
@@ -1,6 +0,0 @@
-## ----install, eval=FALSE------------------------------------------------------
-#  install.packages("fivethirtyeightdata",
-#    repos = "https://fivethirtyeightdata.github.io/drat/",
-#    type = "source")
-#  vignette(package = "fivethirtyeightdata")
-
